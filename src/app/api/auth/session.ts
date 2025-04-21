d6749ef8db6f510ee09c3defd9d38243e5973a4b
apmanager <121115177+apmanager001@users.noreply.github.com>
2025-04-21 18:34:42 -0400

    update

--- src/app/api/auth/session.ts
+++ src/app/api/auth/session.ts
@@ -5,12 +5,11 @@ export const session = async ({ session, token }: any) => {
   return session;
 };
 
-export const getUserSession = async (): Promise<User> => {
+export const getUserSession = async (): Promise<User | null> => {
   const authUserSession = await getServerSession({
     callbacks: {
       session,
     },
   });
-  // if (!authUserSession) throw new Error('unauthorized')
   return authUserSession?.user || null;
 };
