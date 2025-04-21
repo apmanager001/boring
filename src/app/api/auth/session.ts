159335a48e7f97124e31df6442a616156fa0d852
apmanager <121115177+apmanager001@users.noreply.github.com>
2025-04-21 18:33:11 -0400

    updated google sign in

--- src/app/api/auth/session.ts
+++ src/app/api/auth/session.ts
@@ -1,29 +1,16 @@
 import { User, getServerSession } from "next-auth";
 
-// export const session = async ({ session, token }: any) => {
-//   session.user.id = token.id;
-//   return session;
-// };
+export const session = async ({ session, token }: any) => {
+  session.user.id = token.id;
+  return session;
+};
 
-// export const getUserSession = async (): Promise<User> => {
-//   const authUserSession = await getServerSession({
-//     callbacks: {
-//       session,
-//     },
-//   });
+export const getUserSession = async (): Promise<User> => {
+  const authUserSession = await getServerSession({
+    callbacks: {
+      session,
+    },
+  });
   // if (!authUserSession) throw new Error('unauthorized')
-//   return authUserSession?.user;
-// };
-// export const getUserSession = async (): Promise<User> => {
-//   const authUserSession = await getServerSession();
-//   return authUserSession?.user
-// }
-export const getUserSession = async (): Promise<User | null> => {
-  const authUserSession = await getServerSession();
-
-  if (!authUserSession || !authUserSession.user) {
-    return null; // Return null if no session or user is found
-  }
-
-  return authUserSession.user; // Safely return the user object
+  return authUserSession?.user || null;
 };
