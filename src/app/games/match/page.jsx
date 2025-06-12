f34069d00a6702fee22558c75d7c808b29f61c1e
apmanager <121115177+apmanager001@users.noreply.github.com>
2025-06-12 17:14:47 -0400

    update

--- src/app/games/match/page.jsx
+++ src/app/games/match/page.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import Match from './comp/match'
-import { games } from "../../../components/utility/gameList";
+// import { games } from "../../../components/utility/gameList";
 
 export async function generateMetadata() {
-  const seo = games[2];
+  // const seo = games[2];
 
   return {
-    title: seo.name,
-    description: seo.description,
+    title: "Memory Match",
+    description: "Test your memory by matching pairs of cards",
     openGraph: {
-      title: seo.name,
-      description: seo.description,
-      images: [`https://boringsquirrel.com${seo.image}`],
-      url: `https://boringsquirrel.com${seo.link}`,
+      title: "Memory Match",
+      description: "Test your memory by matching pairs of cards",
+      images: [`https://boringsquirrel.com/hp/match.png`],
+      url: `https://boringsquirrel.com/games/match`,
     },
     twitter: {
       card: "summary_large_image",
